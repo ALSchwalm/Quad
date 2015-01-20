9a6009bad49f193a6f9f025d144c9fad9b15e953
Adam Schwalm <adamschwalm@gmail.com>
2015-01-19 19:12:12 -0600

    Spawn a new block immediately after one is placed

--- js/app/config.js
+++ js/app/config.js
@@ -9,18 +9,21 @@ define(function(){
      * @alias module:app/config
      * @namespace
      * @property {object}  config                   - Configuration options
-     * @property {number}  config.game              - General game configuration
-     * @property {string}  config.game.width        - Width of the canvas in px
-     * @property {object}  config.game.height       - Height of the canvas in px
-
-     * @property {number}  config.grid              - Grid settings
+     * @property {object}  config.game              - General game configuration
+     * @property {number}  config.game.width        - Width of the canvas in px
+     * @property {number}  config.game.height       - Height of the canvas in px
+     *
+     * @property {object}  config.grid              - Grid settings
      * @property {number}  config.grid.size         - Width/Height of the grid in px
      * @property {number}  config.grid.numCells     - Number of cells per row/column
      * @property {number}  config.grid.linesVisible - Hide/show grid lines
-
-     * @property {number}  config.color             - Color options
-     * @property {number}  config.grid.unbreakable  - Color of the central unbreakable blocks
-     * @property {number}  config.grid.available    - List of colors used for levels in ascending order
+     *
+     * @property {object}  config.color             - Color options
+     * @property {number}  config.color.unbreakable  - Color of the central unbreakable blocks
+     * @property {number}  config.color.available    - List of colors used for levels in ascending order
+     *
+     * @property {object}  config.generator              - Generator settings
+     * @property {number}  config.generator.defaultWait  - Time in seconds to wait before dropping a quad
      */
     var config = {
         game : {
@@ -35,6 +38,9 @@ define(function(){
         color : {
             unbreakable : 0xFFFFFF,
             available : [0xFF8C00, 0x00008B]
+        },
+        generator : {
+            defaultWait : 4
         }
     };
 

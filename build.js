860182121d7d5af3a87fc3584335454833cd447c
Adam Schwalm <adamschwalm@gmail.com>
2016-04-28 21:35:00 -0500

    Add almond to avoid require js load

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 ({
     baseUrl: 'js/lib',
+    mainConfigFile: 'app.js',
     paths: {
         // Phaser does not fully support require js by default, so
         // we need to explicitly add it to the config
@@ -8,6 +9,8 @@
         // All application files can be required with 'app/<name>'
         app: '../app'
     },
+
     name : "../../app",
+    include: ['../../bower_components/almond/almond'],
     out : "quad-built.js"
 })
